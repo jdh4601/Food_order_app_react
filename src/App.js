3d1b3757c5df87a2d55670ef3b648520f4a1e9b6
DongHyun Jung <jdh9490@gmail.com>
2022-10-03 20:08:32 +0900

    feat: create new modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route path="/cart" element={<Cart />} /> */}
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './Layout/Header';
 import Meals from './Meals/Meals';
 import Cart from './Cart/Cart';
 
 const Home = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const closeCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={closeCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
